09103a88cb489522516e94a5f40efca7dfc26c8c
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-04-03 22:23:12 +0530

    add cron jobs

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { ConnectDB } from "./lib/db.js";
 import cors from "cors";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json()); //Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
 app.use(cors());
+job.start();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
